fccd1a36b105398f1a7d2761cc7aba1371d898f6
Karthik <karthikk1162@gmail.com>
2025-11-30 18:00:15 +0530

    Added settings

--- Virtual_Keyboard.py
+++ Virtual_Keyboard.py
@@ -4,3 +4,19 @@ from pynput.keyboard import Controller, Key
 import time
 import math
 from datetime import datetime
+
+# --- SETTINGS ---
+CAM_WIDTH, CAM_HEIGHT = 1280, 720
+CLICK_SENSITIVITY = 40  # Slightly lower for thumb pinch accuracy
+CLICK_COOLDOWN = 0.20 
+
+THEME = {
+    'bg': (25, 25, 25), 
+    'key': (50, 50, 50), 
+    'hover': (70, 70, 70), 
+    'click': (0, 255, 127), 
+    'shift_active': (255, 100, 100),
+    'save_btn': (138, 43, 226),      # Purple color for Save button
+    'text': (255, 255, 255), 
+    'border': (200, 200, 200)
+}
